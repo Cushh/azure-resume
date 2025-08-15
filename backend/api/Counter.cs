e7c601bc5a23a9ac075c486fa621b8288a7c7e5d
Cushh <sports875@yahoo.com>
2025-08-15 08:27:45 -0400

    Add Azure Static Web Apps workflow

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -5,10 +5,11 @@ namespace Company.Function
     public class Counter
     {
         [JsonProperty(PropertyName="id")]
-    public string Id { get; set; }
-            [JsonProperty(PropertyName="count")]
+    public string Id {get;set;}
+    
+    [JsonProperty(PropertyName = "count")]
 
-    public int Count {get; set;}
+    public int Count {get;set;}
     }
 
 }
